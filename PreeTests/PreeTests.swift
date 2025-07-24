413c72dcb1a4472c439821303cd9f733d529173f
blackcode2 <doky03115@gmail.com>
2025-07-24 20:18:21 +0900

    Initial Commit

---
+++ PreeTests/PreeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PreeTests.swift
+//  PreeTests
+//
+//  Created by KimDogyung on 7/24/25.
+//
+
+import Testing
+@testable import Pree
+
+struct PreeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
