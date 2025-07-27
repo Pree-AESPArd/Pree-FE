d0f7d7c1d443365feef2ca1ecb252331fe47761c
blackcode2 <doky03115@gmail.com>
2025-07-27 17:16:11 +0900

    [UI]: PrimaryButton Component

---
+++ Pree/Features/Camera/Presentation/Views/CameraView.swift
@@ -0,0 +1,25 @@
+//
+//  CameraView.swift
+//  Pree
+//
+//  Created by KimDogyung on 7/27/25.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        ZStack {
+            
+        }
+    }
+}
+
+
+
+
+
+
+#Preview {
+    CameraView()
+}
