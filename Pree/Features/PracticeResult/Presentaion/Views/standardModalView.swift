35813d1d79d01a77bce0367fda24c94620212eee
LYH513 <peperony367@gmail.com>
2025-08-02 22:21:09 +0900

    [UI]: 리포트 결과 UI 3차 완료

---
+++ Pree/Features/PracticeResult/Presentaion/Views/standardModalView.swift
@@ -0,0 +1,18 @@
+//
+//  standardModalView.swift
+//  Pree
+//
+//  Created by 이유현 on 8/2/25.
+//
+
+import SwiftUI
+
+struct standardModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    standardModalView()
+}
