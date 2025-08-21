badcbd9ee2473380e224302b77646774cd78eeeb
blackcode2 <doky03115@gmail.com>
2025-08-21 18:17:25 +0900

    [feat]: Presentation entity 추가

---
+++ Pree/Core/Domain/Entities/Presentation.swift
@@ -0,0 +1,17 @@
+//
+//  Presentation.swift
+//  Pree
+//
+//  Created by KimDogyung on 8/21/25.
+//
+
+import Foundation
+
+struct Presentation: Codable {
+    var userId: String?
+    var presentationName: String?
+    var idealMinTime: Double?
+    var idealMaxTime: Double?
+    var showTimeOnScreen: Bool?
+    var showMeOnScreen: Bool?
+}
